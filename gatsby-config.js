39963b14c357334ccb7f822c0d550e0d3cf27e48
Gaurab Kecy <gaurab.kecy@indmeas.com>
2024-01-04 19:52:56 +0200

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    siteMetadata: {
+        siteUrl: "https://www.mydomain.tld",
+        title: "Gatsby Intro",
+        description: "Frontend masters into to gastsby projects",
+        image:
+            'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+    }
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default function AboutPage() {
+    return (
+        <main>
+            <Link to="/">Home</Link>
+            <h1>About this site</h1>
+        </main>
+    )
+}
